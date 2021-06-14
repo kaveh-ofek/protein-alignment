8be403be128ac29d81ad6cfd704d9339cfe8e275
Ofek Kaveh <kaofek@gmail.com>
2021-06-15 00:34:23 +0300

    simple protein structural alingment

---
+++ README.txt
@@ -0,0 +1,3 @@
+Simple script that takes two PDB identifiers and chain identifiers as input.
+It will retrieve the appropriate files from the PDB and align the chains from each file by their set of C-alpha atoms,
+including the calculation of the RMSD between these atoms.
---
+++ myalign.py
@@ -0,0 +1,110 @@
+import sys
+import Bio.PDB
+from Bio.PDB import MMCIFIO
+from Bio.PDB.PDBParser import PDBParser
+from Bio.PDB.PDBList import PDBList
+
+
+def alignment_method():
+    """
+    method to align 2 structures from pdb together
+    one time 1 on top of 2, and then 2 on top of 1
+    :return: none
+    """
+    # given arguments readr
+    arguments = sys.argv
+    pdb_id1, chain1, pdb_id2, chain2 = arguments[1], arguments[2], arguments[
+        3], arguments[4]
+    # parser of pdb files
+
+    # download and get structure methods
+    download_structures(pdb_id1, pdb_id2)
+
+    # get structures themselves
+    structure_1, structure_2 = get_structures(pdb_id1, pdb_id2)
+    model_1, model_2 = structure_1[0], structure_2[0]
+
+    # CA chains arrays
+    CA_in_1 = get_CAs(model_1, chain1)
+    CA_in_2 = get_CAs(model_2, chain2)
+
+    alignment_object = Bio.PDB.Superimposer()
+
+    # align structure_1 on top of structure_2
+    alignment_object.set_atoms(CA_in_1, CA_in_2)
+    alignment_object.apply(model_2.get_atoms())
+    # RMSD of first alignment
+    print("RMSD of " + pdb_id1 + " on top of " + pdb_id2 + ": "
+          + '{:.20f}'.format(alignment_object.rms))
+
+    # align structure_2 on top of structure_1
+    alignment_object.set_atoms(CA_in_2, CA_in_1)
+    alignment_object.apply(model_1.get_atoms())
+    # RMSD of second alignment
+    print("RMSD of " + pdb_id2 + " on top of " + pdb_id1 + ": "
+          + '{:.20f}'.format(alignment_object.rms))
+
+    mmCIF_output = MMCIFIO()
+
+    # save alignment on top of structure1 as a sif file
+    mmCIF_output.set_structure(structure_1)
+    mmCIF_output.save(pdb_id1 + ".cif")
+
+    # save alignment on top of structure2 as a sif file
+    mmCIF_output.set_structure(structure_2)
+    mmCIF_output.save(pdb_id2 + ".cif")
+
+
+def download_structures(code1, code2):
+    """
+    method to download structures from pdb sitr
+    :param code1: structure 1 to download
+    :param code2: structure 2 to download
+    :return: none
+    """
+    # download object to download from pdb net and saving in current folder
+    downloader = PDBList()
+    downloader.retrieve_pdb_file(code1, file_format="pdb", pdir="./",
+                                 overwrite=True)
+    downloader.retrieve_pdb_file(code2, file_format="pdb", pdir="./",
+                                 overwrite=True)
+
+
+def get_structures(pdb_id1, pdb_id2):
+    """
+    method to get structures of pdb files which we downloaded
+    :param pdb_id1: code of structure1
+    :param pdb_id2: code of structure2
+    :return: structure object of 1 and 2
+    """
+    # parsing object
+    parser = PDBParser(PERMISSIVE=1, QUIET=True)
+    # given structures
+    structure_1 = parser.get_structure("pdb1", "pdb" + pdb_id1 + ".ent")
+    structure_2 = parser.get_structure("pdb2", "pdb" + pdb_id2 + ".ent")
+    return structure_1, structure_2
+
+
+def get_CAs(structure, chain):
+    """
+    method to get CA chains of current given structure
+    :param structure: pdb structure
+    :param chain: the chain we want to read the CA's from
+    :return: array of CAs in structure
+    """
+    # CA atoms list of structure
+    CA_in_struct = []
+    # go through structure chains
+    for ch in structure:
+        # if chain id fits the chain we go through
+        if ch.id == chain:
+            for residue in ch:
+                # and if it includes an CA atom we append it
+                if 'CA' in residue:
+                    CA_in_struct.append(residue['CA'])
+    # return CA atoms
+    return CA_in_struct
+
+
+if __name__ == '__main__':
+    alignment_method()
